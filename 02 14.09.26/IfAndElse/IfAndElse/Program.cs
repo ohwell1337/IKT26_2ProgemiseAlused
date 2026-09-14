b3ac27fbf7bde6f3a68a310c15f4fdad2cb65737
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-14 09:58:09 +0300

    IfAndElse

---
+++ 02 14.09.26/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi");
+
+            // siin on muutuja nimega,
+            // mis on tuubiga string
+            // loeb andmeid konsoolist ja salvestab 
+            // need muutuja sisse
+            string name = Console.ReadLine();
+
+            // kui muutuja name on tühi, siis väljastab konsoolile
+            if (name != "")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("Tere, " + name);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Tere, tundmatu! ERROR");
+                Console.Beep();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
